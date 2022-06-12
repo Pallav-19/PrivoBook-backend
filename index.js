72e2c246074dbf10c60e9ea74f97a3ae8ecdfd82
Pallav <pallavinvizag@gmail.com>
2022-06-13 04:19:57 +0530

    deployment v1.0

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const db = require("./db.config");
 const express = require("express");
 const app = express();
+const path = require('path')
 const bodyParser = require("body-parser");
 const port =  process.env.PORT || 8080;
 const Cors = require("cors");
@@ -18,6 +19,10 @@ const authRoutes = require("./routes/auth.js");
 app.use("/api/auth/", authRoutes);
 const noteRoutes = require("./routes/notes.js");
 app.use("/api/note/", noteRoutes);
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "react", "index.html"));
+//   next();
+// });
 app.listen(port, () => {
   console.log(`listening on http://localhost:${port}/`);
 });
